956c44c72085cac13f3f2398903be6e90d961836
Kirill Fedotov <knprcta@yandex.ru>
2022-07-22 20:36:18 +0300

    feat: добавить компонеты лифта и этажа

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
+<script setup>
+import { reactive } from 'vue';
+import TheElevator from './components/TheElevator.vue';
+import TheLevel from './components/TheLevel.vue';
+
+const state = reactive({
+  elevatorsTotal: 1,
+  levelsTotal: 5,
+});
+</script>
+
 <template>
-  <h1 class="text-3xl text-red-500">Hello World</h1>
+  <div
+    class="flex flex-col w-full h-[85vh] px-3 md:pb-8 md:px-8 xl:px-20 xl:pb-20 xl:pt-5 md:h-screen"
+  >
+    <div class="flex flex-row h-full">
+      <TheElevator
+        v-for="item in state.elevatorsTotal"
+        ref="elevatorList"
+        :key="item"
+        :number="item"
+        :levelsTotal="state.levelsTotal"
+      />
+      <div class="flex flex-col-reverse flex-grow">
+        <TheLevel
+          v-for="item in state.levelsTotal"
+          ref="levelList"
+          :key="item"
+          :number="item"
+        />
+      </div>
+    </div>
+  </div>
 </template>
---
+++ src/components/TheLevel.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps, defineExpose, reactive, toRefs } from 'vue';
+
+const props = defineProps({
+  number: Number,
+});
+
+const state = reactive({
+  isRequested: false,
+});
+
+defineExpose({
+  ...toRefs(props),
+  ...toRefs(state),
+});
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-start justify-center flex-grow px-3 bg-gray-300 border-2 border-white md:px-6 flex-2"
+  >
+    <div class="flex items-center gap-4">
+      <button
+        @click="$emit('call', number)"
+        :class="[
+          {
+            'text-blue-500 border-blue-500 ring-blue-500/50 animate-push scale-90 cursor-wait hover:bg-transparent':
+              state.isRequested,
+          },
+          'flex p-2.5 border-2 border-black rounded-full hover:bg-gray-400 ring-black/50',
+        ]"
+      >
+        <span class="w-3 h-3 bg-current rounded-full"></span>
+      </button>
+      <span class="text-lg font-semibold">{{ number }}</span>
+    </div>
+  </div>
+</template>
