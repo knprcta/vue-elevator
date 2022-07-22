2863a06ae3f804b3352a93dd3d646660902f7037
Kirill Fedotov <knprcta@yandex.ru>
2022-07-22 20:50:13 +0300

    feat: реализовать логику движения лифтов

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <script setup>
-import { reactive } from 'vue';
+import { ref, reactive, watchEffect } from 'vue';
 import TheElevator from './components/TheElevator.vue';
 import TheLevel from './components/TheLevel.vue';
 
+const elevatorList = ref([]);
+const levelList = ref([]);
+
 const state = reactive({
-  elevatorsTotal: 1,
-  levelsTotal: 5,
+  elevatorsTotal: 3,
+  levelsTotal: 7,
+  queue: [],
+});
+
+function call(level) {
+  if (
+    !elevatorList.value.some(
+      (item) => item.currentLevel === level && item.isIdle
+    ) &&
+    !elevatorList.value.some((item) => item.nextLevel === level) &&
+    !state.queue.includes(level)
+  ) {
+    const i = levelList.value.findIndex((item) => item.number === level);
+    levelList.value[i].isRequested = true;
+    state.queue.push(level);
+  }
+}
+
+function stop(level) {
+  const i = levelList.value.findIndex((item) => item.number === level);
+  levelList.value[i].isRequested = false;
+}
+
+function run() {
+  const freeElevatorsArr = elevatorList.value.filter((item) => item.isIdle);
+  const levelsOfFreeElevsArr = freeElevatorsArr
+    .map((item) => (item = item.currentLevel))
+    .sort((a, b) => b - a);
+  const closestLevel = levelsOfFreeElevsArr.reduce((a, b) => {
+    return Math.abs(b - state.queue[0]) < Math.abs(a - state.queue[0]) ? b : a;
+  });
+  const i = elevatorList.value.findIndex(
+    (item) => item.currentLevel === closestLevel && item.isIdle
+  );
+  elevatorList.value[i].nextLevel = state.queue.shift();
+}
+
+watchEffect(() => {
+  if (state.queue.length && elevatorList.value.some((item) => item.isIdle)) {
+    run();
+  }
 });
 </script>
 
@@ -20,6 +63,7 @@ const state = reactive({
         :key="item"
         :number="item"
         :levelsTotal="state.levelsTotal"
+        @stop="stop"
       />
       <div class="flex flex-col-reverse flex-grow">
         <TheLevel
@@ -27,6 +71,7 @@ const state = reactive({
           ref="levelList"
           :key="item"
           :number="item"
+          @call="call"
         />
       </div>
     </div>
